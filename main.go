6dc9f2d17bc51023d6490ccaf756dfeed64a200b
Tomer Golan <tomer.g2@gmail.com>
2023-08-19 18:41:55 +0300

    add logging mw

--- main.go
+++ main.go
@@ -5,12 +5,14 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
 	"github.com/tomerg2/mini-hacker-news/api/routes"
+	"github.com/tomerg2/mini-hacker-news/middlewares"
 	"os"
 )
 
 func main() {
 	router := gin.Default()
 	servicePort := os.Getenv("SERVICE_PORT")
+	router.Use(middlewares.LoggingMiddleware())
 
 	// Initialize routes
 	routes.InitializeRoutes(router)
