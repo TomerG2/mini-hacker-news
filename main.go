3e4f62486c41c5b63d7e00139cf30c3ee05d96ff
Tomer Golan <tomer.g2@gmail.com>
2023-08-19 18:03:22 +0300

    move service port to env var

--- main.go
+++ main.go
@@ -1,18 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
 	"github.com/tomerg2/mini-hacker-news/api/routes"
+	"os"
 )
 
 func main() {
 	router := gin.Default()
+	servicePort := os.Getenv("SERVICE_PORT")
 
 	// Initialize routes
 	routes.InitializeRoutes(router)
 
-	if err := router.Run(":8080"); err != nil {
+	if err := router.Run(fmt.Sprintf(":%s", servicePort)); err != nil {
 		logrus.Fatal("Failed to start the server")
 	}
 }
